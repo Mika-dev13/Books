17b440a4a3594ced863d6f1e814a4d6b62e3b36c
Chorro Michael <michaelchorro.dev@gmail.com>
2022-09-23 11:55:06 +0200

    install react + integration css/html

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Footer from "./components/Footer";
+import NavBar from "./components/NavBar";
+import AddBooks from "./containers/AddBooks";
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <AddBooks />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
