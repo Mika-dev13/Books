df9d57843c0e085708dd04f70285470e3d7fd2f8
Chorro Michael <michaelchorro.dev@gmail.com>
2022-09-24 15:11:02 +0200

    retrieve input value + implementation reducer

--- src/containers/AddBooks.js
+++ src/containers/AddBooks.js
@@ -1,38 +1,72 @@
-import React from 'react'
-
+import React, { useState } from 'react'
 
 const AddBooks = () => {
+  const initialState = {
+    title: '',
+    author: '',
+  }
+
+  const [newData, setNewData] = useState(initialState)
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(newData)
+  }
   return (
     <main role="main">
-        <div className="container-fluid bg-light p-5 d-flex justify-content-center mb-4">
-            <div className='col-8 text-center'>
-                <h1 className='display-4'>BOOKS</h1>
-                <p>Ajouter un livre à votre bibliothèque</p>
-                <form className='row justify-content-center'>
-                    <div className='col'>
-                        <input type="text" className='form-control' placeholder='Titre' required />
-                    </div>
-                    <div className='col'>
-                        <input type="text" className='form-control' placeholder='Auteur' required />
-                    </div>
-                    <div className='col-3 d-grid'>
-                        <button className='btn btn-outline-secondary'>Ajouter un livre</button>
-                    </div>
-                </form>
+      <div className="container-fluid bg-light p-5 d-flex justify-content-center mb-4">
+        <div className="col-8 text-center">
+          <h1 className="display-4">BOOKS</h1>
+          <p>Ajouter un livre à votre bibliothèque</p>
+          <form className="row justify-content-center" onSubmit={handleSubmit}>
+            <div className="col">
+              <input
+                value={newData.title}
+                type="text"
+                className="form-control"
+                placeholder="Titre"
+                required
+                onChange={(e) =>
+                  setNewData({ ...newData, title: e.target.value })
+                }
+              />
             </div>
-        </div>
-        <div className='container' style={{minHeight:'200px'}}>
-            <div className='row'>
-                <div className='col-md-12'>
-                    <ul className='list-group'>
-                        <li className='list-group-item list-group-light d-flex justify-content-between'>Livres enregistrés ici</li>
-                    </ul>
-                </div>
-                <div className='d-flex justify-content-center'>
-                    <button className='btn btn-danger mt-4 mb-5'>Effacer tous les livres</button>
-                </div>
+            <div className="col">
+              <input
+                value={newData.author}
+                type="text"
+                className="form-control"
+                placeholder="Auteur"
+                required
+                onChange={(e) =>
+                  setNewData({ ...newData, author: e.target.value })
+                }
+              />
             </div>
+            <div className="col-3 d-grid">
+              <button className="btn btn-outline-secondary">
+                Ajouter un livre
+              </button>
+            </div>
+          </form>
         </div>
+      </div>
+      <div className="container" style={{ minHeight: '200px' }}>
+        <div className="row">
+          <div className="col-md-12">
+            <ul className="list-group">
+              <li className="list-group-item list-group-light d-flex justify-content-between">
+                Livres enregistrés ici
+              </li>
+            </ul>
+          </div>
+          <div className="d-flex justify-content-center">
+            <button className="btn btn-danger mt-4 mb-5">
+              Effacer tous les livres
+            </button>
+          </div>
+        </div>
+      </div>
     </main>
   )
 }
---
+++ src/redux/reducers/reducerAddBooks.js
@@ -0,0 +1,34 @@
+import { ADD_BOOKS } from './constants'
+import { v4 as uuiv4 } from 'uuid'
+
+const initialState = {
+  books: [],
+}
+
+const helperAddData = (action) => {
+  return {
+    id: uuiv4(),
+    title: action.payload.title,
+    author: action.payload.author,
+  }
+}
+
+//Reducer
+
+const reducerAddBooks = (state = initialState.books, action) => {
+  if (localStorage.getItem('booksData')) {
+    state = JSON.parse(localStorage.getItem('booksData'))
+  }
+
+  switch (action.type) {
+    case ADD_BOOKS:
+      state = [...state, helperAddData(action)]
+      localStorage.setItem('booksData', JSON.stringify(state))
+      return state
+
+    default:
+      break
+  }
+}
+
+export default reducerAddBooks
